b3524c722ed99e6dad8cca65dba41b09fed50c48
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-02-07 18:50:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
-from telegram import Update
-from telegram.ext import filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import pikepdf
 import os
 from flask import Flask, jsonify
@@ -8,25 +7,26 @@ import threading
 
 TOKEN = '6239054864:AAGrtQ4d9_lzH0eOrrUEmtAdpFWs8sw7I2c'
 
+
 app = Flask(__name__)
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext):
     update.message.reply_text('Kirimkan file PDF yang ingin Anda kompres.')
 
-def compress_pdf(file_path: str) -> str:
+def compress_pdf(file_path: str) -> str:  # Added return type
     output_path = 'compressed_' + os.path.basename(file_path)
     # Menggunakan pikepdf untuk mengompres PDF
     with pikepdf.open(file_path) as pdf:
         pdf.save(output_path, compress_streams=True)
     return output_path
 
-def handle_document(update: Update, context: CallbackContext) -> None:
+def handle_document(update: Update, context: CallbackContext):
     document = update.message.document
     file = document.get_file()
     file_path = file.download()
-    
+
     compressed_file_path = compress_pdf(file_path)
-    
+
     with open(compressed_file_path, 'rb') as compressed_file:
         update.message.reply_document(document=compressed_file)
 
@@ -42,10 +42,9 @@ def run_flask():
 
 def main() -> None:
     updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.document.mime_type("application/pdf"), handle_document))
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.document.mime_type("application/pdf"), handle_document))
 
     updater.start_polling()
 
